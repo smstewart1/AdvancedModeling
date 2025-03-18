9093042d4859951bf2fd56c3e129280ad509b773
smstewart1 <s.michael.stewart.76@gmail.com>
2025-03-18 08:15:41 -0400

    debugged and verified

--- AdvancedModeling.py
+++ AdvancedModeling.py
@@ -42,15 +42,15 @@ def custom_clean_up(dataframe: pd) -> pd:
         return dataframe
     
     # remove NAs
-    dataframe.drop_na(inplace = True)
-    
+    dataframe.dropna(inplace = True)
+    dataframe.replace(',', '', regex = True, inplace = True)
+        
+    #convert columns to numeric except the first date column
+    for i in ["Open", "Close", "High", "Low"]:
+        dataframe[i] = dataframe[i].astype(float)
+        
     #create a difference columns
     dataframe["Difference"] = dataframe["Close"] - dataframe["Open"]
-    
-    #convert columns to numeric except the first date column
-    for i in range(2, len(dataframe.columns)):
-        dataframe.iloc[:,i] = dataframe.iloc[:,i].astype(float)
-        
     return dataframe
 
     #develops and returns correlations with correplation plots
@@ -71,35 +71,26 @@ def pre_screen_information(dataframe: pd, output_prefix: str) -> None:
     #creates correlation plots of all the data
     columns_names: list = df_subset.columns.tolist()
     length = len(columns_names)
-    for i in range(1, length - 1):
+    for i in range(0, length - 1):
         for j in range(i + 1, length):
-            plotter_support(df_subset.iloc[:, i], df_subset.iloc[:, j], output_prefix)
+            plotter_support(df_subset[columns_names[i]], df_subset[columns_names[j]], columns_names[i], columns_names[j], output_prefix)
     return
 
     #create plots
-def plotter_support(df1: pd, df2: pd, name: str) -> None:
+def plotter_support(df1: pd, df2: pd, df1_name: str, df2_name: str, name: str) -> None:
     #create labels
-    x_label: list[str] = df1.column.tolist()
-    y_label: list[str] = df2.column.tolist()
-    file_label = f"{name}_{x_label[0]}_vs_{y_label[0]}.tiff"
-    
-    #extracts data
-    x_data: np = df1.DataFrame.to_numpy
-    y_data: np = df2.DataFrame.to_numpy
-    
+    file_label = f"{name}_{df1_name}_vs_{df2_name}.tiff"
+       
     #generates plos
-    plot.scatter(x_data, y_data)
-    plot.xlabel(x_label[0])
-    plot.ylabel(y_label[0])
-    plot.title(f"{x_label}_vs_{y_label}")
+    plot.scatter(df1, df2)
+    plot.xlabel(df1_name)
+    plot.ylabel(df2_name)
+    plot.title(f"{df1_name} vs {df2_name}")
     plot.savefig(file_label)
     
     #cleans up memory
-    plot.clf
-    del x_data
-    del y_data 
-    del x_label
-    del y_label
+    plot.clf()
+    plot.close()
     del file_label
     return
 
@@ -112,7 +103,7 @@ def phase_plot_data(dataframe: pd, phases: int) -> list:
     initial_list: list = dataframe.tolist()
     return_list: list = []
     length: int = len(dataframe)    
-    for i in range(0, phases - 1):
+    for i in range(0, phases):
         return_list.append(initial_list[i:length - phases + i])
     
     return return_list
@@ -120,14 +111,13 @@ def phase_plot_data(dataframe: pd, phases: int) -> list:
     #create phase plots
 def create_phase_lots(data: pd, title: str) -> None:
     #generate phase plots
-    
-    TDPhaseData = phase_plot_data(data, 2)
-    two_D_phase_plot(TDPhaseData, title)
-    del TDPhaseData
-    
-    TDPhaseData = phase_plot_data(data, 3)
-    three_D_phase_plot(TDPhaseData, title)
-    del TDPhaseData
+    for i in ["Open", "Close", "High", "Low"]:
+        TDPhaseData = phase_plot_data(data[i], 2)
+        two_D_phase_plot(TDPhaseData, f"{title} - {i}")
+        del TDPhaseData
+        TDPhaseData = phase_plot_data(data[i], 3)
+        three_D_phase_plot(TDPhaseData, f"{title} - {i}")
+        del TDPhaseData
     
     return
 
@@ -138,14 +128,15 @@ def two_D_phase_plot(data: list, plot_name: str) -> None:
     file_label = f"2Dphase_plot_{plot_name}.tiff"
     
     #generates plots
-    plot.scatter(data[0], data[1])
+    plot.scatter(x = data[0], y = data[1], s = 1)
     plot.xlabel("N")
     plot.ylabel("N + 1" )
-    plot.title(f"Phase Plot")
+    plot.title(f"Phase Plot {plot_name}")
     plot.savefig(file_label)
     
     #cleans up memory
-    plot.clf
+    plot.clf()
+    plot.close()
     del file_label
     return
     
@@ -157,16 +148,17 @@ def three_D_phase_plot(data: list, plot_name: str) -> None:
     
     #generates plots
     fig = plot.figure()
-    ax = fig.add_subplot(rpojection = '3d')
+    ax = fig.add_subplot(projection = '3d')
     ax.scatter(data[0], data[1], data[2])
-    plot.xlabel("N")
-    plot.ylabel("N + 1" )
-    plot.zlabel("N + 2")
-    plot.title(f"3D Phase Plot")
+    ax.set_xlabel("N")
+    ax.set_ylabel("N + 1" )
+    ax.set_zlabel("N + 2")
+    plot.title(f"3D Phase Plot {plot_name}")
     plot.savefig(file_label)
     
     #cleans up memory
-    plot.clf
+    plot.clf()
+    plot.close()
     del fig
     del ax
     del file_label
