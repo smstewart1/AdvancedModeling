e78b7ebdb9cc1638824a63c79886b24fb0ca8f3c
smstewart1 <s.michael.stewart.76@gmail.com>
2025-04-15 10:30:15 -0400

    establishes the sister CPP file

---
+++ CPP_learning.cpp
@@ -0,0 +1,13 @@
+// imported libraries
+#include <iostream>
+    // use names for objects and variables from the standard library
+using namespace std;
+
+// main space
+int main() {
+
+    return 0;
+}
+
+// objects and custom functions
+
